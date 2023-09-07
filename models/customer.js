86a165510b3edd7d196083d39d1b2ea8c33b2d33
spathiyal <spathiyal@gmail.com>
2023-09-07 11:52:07 -0700

    save and fullname code change

--- models/customer.js
+++ models/customer.js
@@ -18,26 +18,26 @@ class Customer {
 
   static async all() {
     const results = await db.query(
-      `SELECT id, 
-         first_name AS "firstName",  
-         last_name AS "lastName", 
-         phone, 
+      `SELECT id,
+         first_name AS "firstName",
+         last_name AS "lastName",
+         phone,
          notes
        FROM customers
        ORDER BY last_name, first_name`
     );
-    return results.rows.map(c => new Customer(c));
+    return results.rows.map((c) => new Customer(c));
   }
 
   /** get a customer by ID. */
 
   static async get(id) {
     const results = await db.query(
-      `SELECT id, 
-         first_name AS "firstName",  
-         last_name AS "lastName", 
-         phone, 
-         notes 
+      `SELECT id,
+         first_name AS "firstName",
+         last_name AS "lastName",
+         phone,
+         notes
         FROM customers WHERE id = $1`,
       [id]
     );
@@ -58,7 +58,10 @@ class Customer {
   async getReservations() {
     return await Reservation.getReservationsForCustomer(this.id);
   }
-
+  // get full name
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
   /** save this customer. */
 
   async save() {
